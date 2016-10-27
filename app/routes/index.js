53ac4d3edee8b51b786c5367d2bf366d8f2cdf2f
Kevin Johnston <kjohnston1149@gmail.com>
2016-10-27 13:19:11 -0700

    Add delete feature to blog-tile

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('blog');
+  },
+
+  actions: {
+    destroyBlog(blog) {
+      blog.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
